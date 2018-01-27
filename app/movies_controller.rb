a65a44440926686205925cc4be550aa4c92cfc8f
Matt Trussell <smtrussell@gmail.com>
2018-01-27 23:24:55 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -53,6 +53,7 @@ def can_find_by_multiple_attributes
   # release_date == 2000
   # director == "Me"
   movie = Movie.where(title: "Title", release_date: 2000, director: "Me")
+  movie
 end
 
 def can_find_using_where_clause_and_be_sorted
